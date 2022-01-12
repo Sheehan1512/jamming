bb5bcf3cdd52245e16cae8393a8fd118d14a9e37
Sheehan1512 <sheehanvy@gmail.com>
2022-01-12 15:02:08 +0600

    search results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,27 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'Save Your Tears',
+          artist: 'Weeknd',
+          album: 'Jani na',
+          id: 1
+        },
+        {
+          name: 'Sorry',
+          artist: 'Justin Bieber',
+          album: 'Jani na bhai',
+          id: 2
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
@@ -12,7 +33,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults} />
             <Playlist />
           </div>
         </div>
