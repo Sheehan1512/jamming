0e41e9523d4b3fc73397d602ca2040e79821e81d
Sheehan1512 <sheehanvy@gmail.com>
2022-01-14 09:20:06 +0600

    save playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -51,6 +51,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
 
   }
 
@@ -74,6 +75,10 @@ class App extends React.Component {
     this.setState({playlistName: name});
   }
 
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -88,7 +93,8 @@ class App extends React.Component {
             <Playlist playlistName={this.state.playlistName}
                       playlistTracks={this.state.playlistTracks} 
                       onRemove={this.removeTrack}
-                      onNameChange={this.updatePlaylistName} />
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist} />
                       
 
           </div>
